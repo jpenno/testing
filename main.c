4cc1cba2dab84c562d00e72ddc3e60c1b2772c23
jp <jpenno123@hotmail.com>
2021-10-29 01:36:51 +1100

    tidy up main

--- main.c
+++ main.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 
 int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
-int add(int x, int y) { return x + y; }
 int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
-int sub(int x, int y) { return x - y; }
+
 int main() {
   printf("Hello World");
-  sub(29, 39);
-  add(29, 39);
-  sub(29, 39);
-  sub(29, 39);
-  add(29, 39);
-  sub(29, 39);
   return 0;
-}
-int doseSomething() {
-  // adding somthing to test
 }
